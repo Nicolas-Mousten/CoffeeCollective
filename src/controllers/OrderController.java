eb0cf0187f33401248fac1bb75526c1339c7216e
Nicolas-Mousten <nicolas.mousten@gmail.com>
2021-11-15 14:13:00 +0100

    Moved Main menu to orderController

--- src/controllers/OrderController.java
+++ src/controllers/OrderController.java
@@ -1,4 +1,41 @@
 package controllers;
 
+import products.*;
+import ui.OrderTakingMenu;
+import ui.StatisticsMenu;
+
 public class OrderController {
+    public static void MainMenu(){
+        OrderTakingMenu orderMenu = new OrderTakingMenu();
+        StatisticsMenu statisticsMenu = new StatisticsMenu();
+
+        orderMenu.welcomeUser();
+        int userInput;
+        CustomerBasket currentCustomer = new CustomerBasket();
+        do{
+            orderMenu.printOptions();
+            userInput = orderMenu.receiveUserInput();
+            switch(userInput){
+                case 1:
+                    currentCustomer.addToAmericano(new Americano());
+                    break;
+                case 2:
+                    currentCustomer.addToEspresso(new Espresso());
+                    break;
+                case 3:
+                    currentCustomer.addToTea(new Tea());
+                    break;
+                case 4:
+                    currentCustomer.addToBaguette(new Baguette());
+                    break;
+                case 5:
+                    currentCustomer.addToCroissant(new Croissant());
+                    break;
+                case 6:
+                    statisticsMenu.printStatisticsChoice();
+                    statisticsMenu.receiveUserInput();
+                    break;
+            }
+        }while(userInput != 9);
+    }
 }
