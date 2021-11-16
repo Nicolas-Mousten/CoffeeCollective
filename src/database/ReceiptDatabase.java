d04e93da653fd83682748ce1013443701ace560b
Nicolas-Mousten <nicolas.mousten@gmail.com>
2021-11-16 09:53:37 +0100

    Recepit finsih

--- src/database/ReceiptDatabase.java
+++ src/database/ReceiptDatabase.java
@@ -1,33 +1,26 @@
 package database;
 
+
 import products.CustomerBasket;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class ReceiptDatabase {
-    public ArrayList<CustomerBasket> memoryList = new ArrayList<>();
+    public static ArrayList<CustomerBasket> memoryList = new ArrayList<>();
 
-    public static void addToDatabase(CustomerBasket currentCustomer){
-        File file = new File("Resources/Receipts.csv");
-        try{
-        Scanner fileScanner = new Scanner(file);
-        while(fileScanner.hasNext()){
-            String line = fileScanner.nextLine();
-            String[] lineArray = line.split(";");
-            ArrayList<Object> tempObject = new ArrayList<Object>(Arrays.asList(lineArray));
-
-
+    public static void addToDatabase(CustomerBasket currentCustomer) {
+        try(FileWriter fw = new FileWriter("Resources/Receipts.csv", true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter out = new PrintWriter(bw)){
+            String inputString = currentCustomer.toString();
+            out.println(inputString);
+        }catch(IOException e){
+            System.out.println("Can't write to this file");
         }
 
-
-
-        }catch(FileNotFoundException e){
-            System.out.println("File not found");
-        }
     }
 }
