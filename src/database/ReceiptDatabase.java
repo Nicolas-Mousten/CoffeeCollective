c585acd0ea24ad81d5d82c97299bd9f4bfa80ac8
Nicolas-Mousten <nicolas.mousten@gmail.com>
2021-11-17 10:29:05 +0100

    Recepit finsih

--- src/database/ReceiptDatabase.java
+++ src/database/ReceiptDatabase.java
@@ -4,10 +4,8 @@ package database;
 import products.CustomerBasket;
 
 import java.io.*;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+
 
 public class ReceiptDatabase {
     public static ArrayList<CustomerBasket> memoryList = new ArrayList<>();
